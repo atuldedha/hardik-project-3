40aab2890f61216efeead6b7d440d3eac7422b48
Atul Choudhary <t-atul1.choudhary@paytmmoney.com>
2023-06-07 15:54:22 +0530

    Contact form, Emailjs and whatsapp added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import ReactWhatsapp from "react-whatsapp";
 import AboutSection from "./components/AboutSection";
 import ContactSection from "./components/ContactSection";
 import Header from "./components/Header";
 import ProductsSection from "./components/ProductsSection";
 
 function Home() {
+  const number = "+918888888888";
   return (
     <div className="bg-bgColor2">
       <div className="bg-bgColor1">
@@ -18,15 +20,25 @@ function Home() {
         </section>
 
         {/* product section */}
-        <section className="mb-8 lg:mb-32">
+        <section className="mb-4 lg:mb-16">
           <ProductsSection />
         </section>
 
         {/* contact section */}
-        <section className="pb-8 lg:pb-32">
+        <section className="bg-bgColor2 pt-4 lg:pt-16 pb-14 lg:pb-36">
           <ContactSection />
         </section>
       </div>
+
+      <ReactWhatsapp number={number}>
+        {/* WhatsApp icon */}
+        <a
+          href="https://wa.me/2348100000000"
+          className="fixed bottom-4 right-4 z-10 flex items-center justify-center w-10 h-10 rounded-[50px] bg-[#25d366] text-white text-base"
+        >
+          <i className="fa fa-whatsapp whatsapp-icon"></i>
+        </a>
+      </ReactWhatsapp>
     </div>
   );
 }
--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,7 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  EnvelopeIcon,
+  MapPinIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/outline";
+import emailjs from "@emailjs/browser";
 
 const ContactSection = () => {
-  return <div>ContactSection</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const [submitButtonDisable, setSubmitButtonDisable] = useState(false);
+
+  const handleChangle = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitButtonDisable(true);
+    emailjs
+      .send(
+        process.env.REACT_APP_EMAIL_SERVICE_ID,
+        process.env.REACT_APP_EMAIL_TEMPLATE_ID,
+        { ...formData },
+        process.env.REACT_APP_EMAIL_PUBLIC_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setFormData({ name: "", email: "", subject: "", message: "" });
+          setSubmitButtonDisable(false);
+        },
+        (error) => {
+          console.log(error.text);
+          setSubmitButtonDisable(false);
+        }
+      )
+      .catch((err) => {
+        console.log(err);
+        setSubmitButtonDisable(false);
+      });
+  };
+  return (
+    <div className="sectionContainer">
+      <div className="flex w-full space-y-10 space-x-0 flex-col lg:flex-row lg:space-y-0 lg:space-x-8">
+        {/* contact left section */}
+        <div className="flex flex-col items-center lg:items-start space-y-6 lg:basis-1/2 w-full">
+          <div className="space-y-2 items-start">
+            <h3 className="heading mb-0 text-center lg:text-left">Contact</h3>
+            <h4 className="text-base md:text-lg xl:text-xl font-semibold text-center">
+              I have just got what you need.
+              <span className="underline decoration-blue7/50 ">{`Let's Talk`}</span>
+            </h4>
+          </div>
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-1 gap-10 place-items-start sm:place-items-center lg:place-items-start items-start mt-5 w-full">
+            <div className="flex items-center space-x-2 md:space-x-5">
+              <PhoneIcon className="animate-pulse text-orange1 h-4 w-4 md:h-7 md:w-7" />
+              <p className="text-sm md:text-base xl:text-lg">+9188888888</p>
+            </div>
+            <div className="flex items-center space-x-2 md:space-x-5">
+              <EnvelopeIcon className="animate-pulse text-orange1 h-4 w-4 md:h-7 md:w-7" />
+              <p className="text-sm md:text-base xl:text-lg">lorem@gmail.com</p>
+            </div>
+            <div className="flex items-center space-x-2 md:space-x-5">
+              <MapPinIcon className="animate-pulse text-orange1 h-4 w-4 md:h-7 md:w-7" />
+              <p className="text-sm md:text-base xl:text-lg">India</p>
+            </div>
+          </div>
+        </div>
+
+        {/* form */}
+        <form
+          action=""
+          className="basis-1/2 flex flex-col items-center w-full"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col w-full space-y-2 max-w-2xl">
+            <div className="flex space-x-2 items-center w-full max-w-2xl">
+              <input
+                name="name"
+                type="text"
+                className="contactInput w-full"
+                placeholder="Name"
+                onChange={handleChangle}
+              />
+              <input
+                name="email"
+                placeholder="Email"
+                onChange={handleChangle}
+                type="email"
+                className="contactInput w-full"
+              />
+            </div>
+            <input
+              name="subject"
+              type="text"
+              placeholder="Subject"
+              onChange={handleChangle}
+              className="contactInput"
+            />
+
+            <textarea
+              name="message"
+              rows={5}
+              placeholder="Message"
+              onChange={handleChangle}
+              className="contactInput"
+            />
+            <button
+              type="submit"
+              disabled={submitButtonDisable}
+              className="px-5 py-3 rounded-md text-white font-bold text-lg cursor-pointer"
+              style={{
+                background:
+                  "linear-gradient(90deg, rgba(79, 172, 254, 0.6) 0%, rgba(0, 242, 254, 0.6) 100%), #2898FF",
+              }}
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactSection;
